f00c945848edfae04b4f538eff2277a9d1b6e962
David <davidnahum92@gmail.com>
2020-12-31 15:28:42 +0200

    firebase fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD-rGrDicCXqNvyXRpX0AUndk5MRTHL6PE",
+    authDomain: "squares-ae581.firebaseapp.com",
+    projectId: "squares-ae581",
+    storageBucket: "squares-ae581.appspot.com",
+    messagingSenderId: "860282218607",
+    appId: "1:860282218607:web:22b6947e27830d22b0459a",
+    measurementId: "G-M8Q2M8SPX6"
+  }
 };
